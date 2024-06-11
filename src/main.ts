05c0df00e472f8fd514d44061e644837c391a243
unknown <kikidotid@gmail.com>
2024-06-11 21:38:16 +0700

    backend kegiatan

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://kegiatan-fe.vercel.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   };
   app.enableCors(corsOptions);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
